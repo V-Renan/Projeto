9b18a5f610d9646fc464137928d023deb9734dc8
V-Renan <victorrenan2018@gmail.com>
2024-01-15 12:52:46 -0300

    interfaces de repositorio

---
+++ biblioteca/src/main/java/repository/EmprestimoRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import model.Emprestimo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmprestimoRepository extends JpaRepository<Emprestimo, Long> {
+}
---
+++ biblioteca/src/main/java/repository/LivroRepository.java
@@ -0,0 +1,6 @@
+package repository;
+
+import model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface LivroRepository extends  JpaRepository<Livro, Long> {
+}
