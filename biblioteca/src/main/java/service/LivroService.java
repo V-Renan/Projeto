40e9ef776d543679b5c2ae4487f84feb302d9091
V-Renan <victorrenan2018@gmail.com>
2024-01-15 12:10:20 -0300

    interface
    metodos relacionados ao livro

--- biblioteca/src/main/java/service/LivroService.java
+++ biblioteca/src/main/java/service/LivroService.java
@@ -1,5 +1,12 @@
 package service;
 
+import model.Livro;
+
+import java.util.List;
+
 public interface LivroService {
 
+    Livro criarLivro(String titulo, String autor, String genero);
+    Livro obterLivroPorID(Long id);
+    List<Livro> obterTodosLivros();
 }
